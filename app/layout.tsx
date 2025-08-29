2a68818dc7283ae5721a5cc33c3dfe9fd9a8de7f
AnonAmosAdmn <122024790+AnonAmosAdmn@users.noreply.github.com>
2025-08-28 21:37:49 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'PE PUNKS Avatar Creator',
+  title: 'APE PUNKS Avatar Creator',
   description: 'Create your own NFT avatar by selecting different traits',
 };
 
